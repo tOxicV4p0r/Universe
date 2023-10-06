b9f120b7e985e77f0df913d1ae70b274530c6913
kraisorn <22193826+tOxicV4p0r@users.noreply.github.com>
2023-10-06 14:39:09 +0700

    add deploy command

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,36 @@
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
 const planets = [];
 
-module.exports = planets;
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6
+}
+
+function loadPlanetData() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream('./data/kepler.csv')
+            .pipe(parse({
+                comment: '#',
+                columns: true,
+            }))
+            .on('data', (data) => {
+                // console.log(data);
+                if (isHabitablePlanet(data))
+                    planets.push(data);
+            })
+            .on('end', () => {
+                console.log('planet that like earth :', planets.length);
+                // console.log('end');
+                resolve();
+            })
+
+    });
+}
+
+module.exports = {
+    planets,
+    loadPlanetData,
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,16 @@
 const http = require('http');
 const app = require('./app');
 
+const { loadPlanetData } = require('./models/planets.model');
 const PORT = process.env.PORT || 4000;
 
 const server = http.createServer(app);
-server.listen(PORT, () => {
-    console.log('Server running on port :', PORT);
-})
+
+async function startServer() {
+    await loadPlanetData();
+    server.listen(PORT, () => {
+        console.log('Server running on port :', PORT);
+    })
+}
+
+startServer();
